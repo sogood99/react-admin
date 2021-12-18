e213d05163e3db290787cba728ba180593c64908
Onii Chan <46487694+sogood99@users.noreply.github.com>
2021-12-18 12:55:15 +0800

    Changed timeonline to component

--- src/components/timeOnline/Timeonline.jsx
+++ src/components/timeOnline/Timeonline.jsx
@@ -6,58 +6,66 @@ import {
   Typography,
 } from "@mui/material";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../../config";
 
-export default function Timeonline() {
-  const [count, setCount] = useState(0);
-  const [start, setStart] = useState(false);
-  useEffect(() => {
-    if (!start) {
-      setStart(true);
-      axios
-        .get(global.config.backendUrl + "/api/admin/get_online_time")
-        .then((res) => {
-          setCount((new Date() - new Date(res.data.date)) / 1000);
-          setInterval(() => {
-            setCount((prevCount) => prevCount + 1);
-          }, 1000);
-        });
-    }
-  }, [start, count]);
-  return (
-    <Card>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          Server Online Time
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Since Last Boot
-        </Typography>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "baseline",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <Typography sx={{ fontSize: 24 }} color="text.primary" gutterBottom>
-            {Math.floor(count / 3600)} Days
+export default class Timeonline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    axios
+      .get(global.config.backendUrl + "/api/admin/get_online_time")
+      .then((res) => {
+        this.setState({ count: (new Date() - new Date(res.data.date)) / 1000 });
+        this.timerId = setInterval(() => {
+          this.setState({ count: this.state.count + 1 });
+        }, 1000);
+      });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const count = this.state.count;
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            Server Online Time
           </Typography>
-          <Typography sx={{ fontSize: 20 }} gutterBottom>
-            {Math.floor((count / 60) % 60)} Minutes
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Since Last Boot
           </Typography>
-          <Typography sx={{ fontSize: 16 }} gutterBottom>
-            {Math.floor(count % 60)} Seconds
-          </Typography>
-        </div>
-      </CardContent>
-      <CardActions>
-        <Link to="/analytics">
-          <Button size="small">Learn More</Button>
-        </Link>
-      </CardActions>
-    </Card>
-  );
+          <div
+            style={{
+              display: "flex",
+              alignItems: "baseline",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Typography sx={{ fontSize: 24 }} color="text.primary" gutterBottom>
+              {Math.floor(count / 3600)} Days
+            </Typography>
+            <Typography sx={{ fontSize: 20 }} gutterBottom>
+              {Math.floor((count / 60) % 60)} Minutes
+            </Typography>
+            <Typography sx={{ fontSize: 16 }} gutterBottom>
+              {Math.floor(count % 60)} Seconds
+            </Typography>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Link to="/analytics">
+            <Button size="small">Learn More</Button>
+          </Link>
+        </CardActions>
+      </Card>
+    );
+  }
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,20 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../config";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const HandleLogin = () => {
+    global.config.isLogin = true;
+    navigate("/");
+  };
+
+  return (
+    <div>
+      Login
+      <Button onClick={HandleLogin}>Login</Button>
+    </div>
+  );
 }
